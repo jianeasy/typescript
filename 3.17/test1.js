92bcebfd7a9e67eae656887f3e1b10619b22115d
jianyi <934384533@qq.com>
2020-03-17 18:40:24 +0800

    更新test.ts

--- 3.17/test1.js
+++ 3.17/test1.js
@@ -1,2 +1,22 @@
 var message = "hello world";
 console.log(message);
+//面向对象
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.name = function () {
+        console.log("xiaoming");
+    };
+    return Person;
+}());
+var p1 = new Person();
+p1.name();
+//数据类型
+//数组
+var arr1 = [1, 2];
+//数组泛型
+var arr2 = [1, 2];
+//元组
+var x;
+x = ["name", "小明"];
+console.log(x[0]);
--- 3.17/test1.ts
+++ 3.17/test1.ts
@@ -1,2 +1,19 @@
-var message:string = "hello world"
-console.log(message)
+var message:string = "hello world";
+console.log(message);
+//面向对象
+class Person{
+    name():void{
+        console.log("xiaoming");
+    }
+}
+var p1 = new Person();
+p1.name();
+//数据类型
+//数组
+let arr1:number[] = [1, 2];
+//数组泛型
+let arr2:Array<number> = [1, 2];
+//元组
+let x:[string, string];
+x = ["name","小明"];
+console.log(x[0]);  
